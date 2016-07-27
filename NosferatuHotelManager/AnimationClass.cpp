48a7d2f2454f400d5d3315811c358c8bb3f9ea2c
NosferatuHotelManager <f844732@mvrht.com>
2016-07-27 17:24:38 -0400

    Further framework for animations.

--- NosferatuHotelManager/AnimationClass.cpp
+++ NosferatuHotelManager/AnimationClass.cpp
@@ -1,2 +1,13 @@
 #include "stdafx.h"
 #include "AnimationClass.h"
+#include "AnimationFrame.h"
+
+bool Animation::LoadAnimation(std::string _fileName)
+{
+	return false;
+}
+
+bool Animation::PlayAnimation(std::string _animationName)
+{
+	return false;
+}
--- NosferatuHotelManager/AnimationClass.h
+++ NosferatuHotelManager/AnimationClass.h
@@ -1,9 +1,12 @@
 #pragma once
+#include "AnimationFrame.h"
 
 //This class will represent a single animation
 class Animation
 {
-	sf::Sprite m_sprite;
+	sf::Sprite												m_sprite;
+	std::map < std::string, std::vector<AnimationFrame>>	m_animations;
 public:
-
+	bool LoadAnimation(std::string _fileName);
+	bool PlayAnimation(std::string _animationName);
 };
