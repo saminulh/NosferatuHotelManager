ff851b01d3464cace2d03b485c973afeb99a0d02
NosferatuHotelManager <f844732@mvrht.com>
2016-07-28 18:31:32 -0400

    Screens manager.

--- NosferatuHotelManager/ScreensManager.h
+++ NosferatuHotelManager/ScreensManager.h
@@ -1,1 +1,13 @@
 #pragma once
+
+//This singleton will be responsible for managing all screens
+class ScreensManager
+{
+	sf::RenderWindow	m_renderWindow;
+public:
+	ScreensManager();
+
+
+
+	sf::RenderWindow& GetWindow();
+};
