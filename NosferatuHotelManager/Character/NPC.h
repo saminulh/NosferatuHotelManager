6753c8858f2d009211e8a528d597d32a6299cab0
Alienwarepc <Alienwarepc@SAMINULHAQUE>
2016-07-28 19:13:23 -0400

    push

--- NosferatuHotelManager/Character/NPC.h
+++ NosferatuHotelManager/Character/NPC.h
@@ -4,19 +4,11 @@
 class NPC : public Character {
 	static std::vector<NPC> NPCStack;
 
-	static NPC getNPCByID(int ID) {
-		return NPCStack[ID];
-	}
+	static NPC getNPCByID(int ID);
+	static void createNPC(std::string type);
 
-	static void createNPC(std::string type) {
-		NPC newNPC = NPC();
-		//ADD TRAITS
-		NPCStack.push_back(newNPC);
-	}
-
-	int characterID;
+private:
 	char personType;
-
 	int basePerceptiveness;
 	int baseFriendliness;
 	int baseSocialNess;
@@ -27,6 +19,19 @@ class NPC : public Character {
 
 	std::map<Character, int> friendlinessModifiers;
 
+public:
+	char getPersonType();
+	int getPerceptiveness();
+	int getBaseFriendliness();
+	int getBaseSocialNess();
+	int getAggressiveness();
+	int getSuspicion();
+	void setSuspicion(int newSuspicionLevel);
+	std::string getAge();
+	bool isMale();
+
+	int getFriendlinessToCharacter(Character character);
+	void setFriendlinessToCharacter(Character character, int newFriendliness);
 
 };
 
