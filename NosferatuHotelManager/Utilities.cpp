56ad07d23f6c948a8b668e7224ebe3d159dca4fb
Alienwarepc <Alienwarepc@SAMINULHAQUE>
2016-07-28 19:13:23 -0400

    push

--- NosferatuHotelManager/Utilities.cpp
+++ NosferatuHotelManager/Utilities.cpp
@@ -5,3 +5,15 @@ int Utilities::randInt(int lowerBound, int upperBound)
 {
 	return 0;
 }
+#include "stdafx.h"
+#include "Utilities.h"
+
+
+Utilities::Utilities()
+{
+}
+
+
+Utilities::~Utilities()
+{
+}
