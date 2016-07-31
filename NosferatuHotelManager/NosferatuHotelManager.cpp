2461856089d95dd4124dd2a5ce85ca37278e42b2
NosferatuHotelManager <f844732@mvrht.com>
2016-07-31 15:11:37 -0400

    Started adding first steps.

--- NosferatuHotelManager/NosferatuHotelManager.cpp
+++ NosferatuHotelManager/NosferatuHotelManager.cpp
@@ -1,6 +1,13 @@
 // NosferatuHotelManager.cpp : Defines the entry point for the console application.
 //
 
+
+/**********************************************************************************/
+/*
+**					MAP EDITOR - NOT THE ACTUAL GAME
+**
+/**********************************************************************************/
+
 #include "stdafx.h"
 #include "AnimationClass.h"
 
@@ -17,58 +24,24 @@ GUIManager			guiManager;
 ActivityManager		activityManager;
 CharacterManager	characterManager;
 
-const int MORNINGSTARTHOUR	= 6;
-const int MORNINGENDHOUR	= 15;
-const int EVENINGSTARTHOUR	= 16;
-const int EVENINGENDHOUR	= 20;
-const int NIGHTSTARTHOUR	= 21;
-const int NIGHTENDHOUR		= 5;
+const int MORNINGSTARTHOUR = 6;
+const int MORNINGENDHOUR = 15;
+const int EVENINGSTARTHOUR = 16;
+const int EVENINGENDHOUR = 20;
+const int NIGHTSTARTHOUR = 21;
+const int NIGHTENDHOUR = 5;
 
+
+
+float					mapX, mapY;
+std::string				mapLoc;
+std::ofstream			file;
+tinyxml2::XMLDocument	doc;
 void Init()
 {
 	debug.Start();
 
 	srand((unsigned int)time(NULL));
-
-
-	Animation anim;
-	anim.LoadAnimation("testAnim.vAnim");
-
-	Animation	vampLoading;
-	vampLoading.LoadAnimation("VampLoadingAnim.vAnim");
-	graphicsManager.LoadTexture("HotelEntrance.png");
-
-	sf::Sprite testBackground;
-	testBackground.setTexture(graphicsManager.GetTexturesMap()["HotelEntrance.png"]);
-
-	vampLoading.BeginAnimation("VampLoadingAnim.vAnim");
-	vampLoading.GetSprite().setPosition(400, 230);
-
-
-	std::vector<std::string> tempButtonAnims;
-	tempButtonAnims.push_back("testButtonDefaultAnim.vAnim");
-	tempButtonAnims.push_back("testButtonMouseOverAnim.vAnim");
-	tempButtonAnims.push_back("testButtonMouseClickedAnim.vAnim");
-
-
-	void(*testOnClick)(void);
-	testOnClick = &ButtonActions::TestButtonAction;
-
-	Button button;
-	button.CreateButton("TEST!", tempButtonAnims, sf::Vector2f(50, 50), testOnClick, sf::Vector2f(40,15));
-	button.GetSprite().setScale((float)1.5, (float)1.5);
-
-	Button playButton;
-	playButton.CreateButton("Play", tempButtonAnims, sf::Vector2f(50, 150), testOnClick, sf::Vector2f(40, 15));
-	playButton.GetSprite().setScale((float)1.5, (float)1.5);
-
-	Button settingsButton;
-	settingsButton.CreateButton("Settings", tempButtonAnims, sf::Vector2f(50, 250), testOnClick, sf::Vector2f(40, 15));
-	settingsButton.GetSprite().setScale((float)1.5, (float)1.5);
-
-	guiManager.GetButtonsMap().insert(std::pair<std::string, Button>("TEST!", button));
-	guiManager.GetButtonsMap().insert(std::pair<std::string, Button>("Play", playButton));
-	guiManager.GetButtonsMap().insert(std::pair<std::string, Button>("Settings", settingsButton));
 
 	//Restart timer so that it's zero when the window actually starts
 	screensManager.m_timer.restart();
@@ -79,41 +52,64 @@ void Init()
 		{
 			screensManager.m_elapsedTime -= screensManager.m_timePerFrame;
 
-		sf::Event event;
-		while (screensManager.GetWindow().pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				screensManager.GetWindow().close();
-		}
+			sf::Event event;
+			while (screensManager.GetWindow().pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					screensManager.GetWindow().close();
+			}
 
+			//Frame Updates
 			screensManager.m_mousePos = sf::Mouse::getPosition(screensManager.GetWindow());
-
-			vampLoading.Update(sf::seconds(screensManager.m_timePerFrame));
-
 			Utilities::Update();
-
 			guiManager.Update();
 
+
+
 			/*********** Clear the screen ******************/
-		screensManager.GetWindow().clear();
-
-
-			screensManager.GetWindow().draw(testBackground);
-			screensManager.GetWindow().draw(vampLoading.GetSprite());
+			screensManager.GetWindow().clear();
 
 			/*********** Draw the GUI to the screen ******************/
 			guiManager.DrawToWindow();
 
+
 			/*********** Display the screen ******************/
-		screensManager.GetWindow().display();
+			screensManager.GetWindow().display();
+		}
 	}
-}
 }
 
 int main()
 {
+	std::cout << "Enter the map's file location: ";
+	std::cin >> mapLoc;
+	
+	tinyxml2::XMLNode* root = doc.NewElement("MapTemplate");
+	doc.InsertFirstChild(root);
+	tinyxml2::XMLElement* element = doc.NewElement("Meta");
+	root->InsertFirstChild(element);
+
+	std::cout << "Enter the map's size x: ";
+	std::cin >> mapX;
+	element->SetAttribute("MapSizeX", mapX);
+
+	std::cout << "Enter the map's size y: ";
+	std::cin >> mapY;
+	element->SetAttribute("MapSizeY", mapY);
+
+	doc.SaveFile("test.xml");
+
+	file.open(mapLoc);
+	if (file.is_open())
+	{
+
+	}
+
+	system("pause");
+
+
 	Init();
 
-    return 0;
+	return 0;
 }
 
