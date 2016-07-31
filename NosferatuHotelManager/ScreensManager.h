a43d5beffa2d7e235fa7229c546daa5edaa01854
NosferatuHotelManager <f844732@mvrht.com>
2016-07-31 09:52:02 -0400

    Screen tags!
    Buttons also don't flop around now unless moused over.

--- NosferatuHotelManager/ScreensManager.h
+++ NosferatuHotelManager/ScreensManager.h
@@ -4,14 +4,27 @@
 //Essentially it will be the global handler for accessing the window
 class ScreensManager
 {
-	sf::RenderWindow	m_renderWindow;
 public:
+	//Used by graphics objects to identify if they should exist/be drawn
+	enum ScreenTag
+	{
+		MainMenu, SettingsMenu
+	};
+
 	const float			m_timePerFrame = 1 / 30.f;
 	float				m_elapsedTime;
 	sf::Vector2i		m_mousePos;
 	sf::Clock			m_timer;
+	
+
 
 	ScreensManager();
 
 	sf::RenderWindow&	GetWindow();
+	ScreenTag			GetCurrentScreenTag();
+	void				ChangeScreenTag(ScreenTag _newTag);
+
+private:
+	sf::RenderWindow	m_renderWindow;
+	ScreenTag			m_currentScreen;
 };
